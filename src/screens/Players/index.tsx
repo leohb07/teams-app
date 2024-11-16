f248b68095bd2c50b822760453ab9f05dc3e32c1
Leonardo Barrocal <leohb1501@gmail.com>
2024-11-16 14:52:09 -0300

    feat: finish layout

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,6 +7,9 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -40,10 +43,29 @@ export function Players() {
               />
             )}
             horizontal
+            showsHorizontalScrollIndicator={false}
           />
 
           <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
         </HeaderList>
+
+        <FlatList
+          data={players}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <PlayerCard name={item} onRemove={() => {}} />
+          )}
+          ListEmptyComponent={() => (
+            <ListEmpty message="Não há pessoas nesse time" />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[
+            { paddingBottom: 100 },
+            players.length === 0 && { flex: 1 },
+          ]}
+        />
+
+        <Button title="Remover turma" type="SECONDARY" />
       </Container>
     </SafeArea>
   );
